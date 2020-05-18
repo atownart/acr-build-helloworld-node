50858a15fcbe496e1836cdec38d2ba332688401d
Adam Town <atown@beyondtrust.com>
2020-05-18 16:48:24 +0100

    yaml task test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World 3 :)\n')
+  response.write('Built from YAML task :)\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
